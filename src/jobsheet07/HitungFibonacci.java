55c0abe8ceb2cb2817678527d3e6802d26ee9079
netammaula <netammaula@Neta>
2018-09-12 11:11:52 +0700

    Initial Commit

--- src/jobsheet07/HitungFibonacci.java
+++ src/jobsheet07/HitungFibonacci.java
@@ -11,20 +11,18 @@ package jobsheet07;
  * @author netammaula
  */
 public class HitungFibonacci {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void tampilJudul(String identitas) {
-        // TODO code application logic here
-        System.out.println("Identitas :"+ identitas);
+    private static void tampilJudul(String identitas)   {
+        System.out.println("Identitas : "+ identitas);
         
         System.out.println("\nHitung Fibonacci");
         System.out.println("1, 1, 2, 3, 5, 8, 13, 21, ... dst.\n");
+  
     }
     
-        String identitas = "Neta Minkhatul Maula / X RPL 3 / 31";
+    public static void main(String[] args)  {
+        
+        String identitas = "Neta Minkhatul Maula / X RPL 3/ 31";
         
         tampilJudul(identitas);
-    
+    }
 }
