6c9796345a9a3d49dbab707cd344358d156b9063
netammaula <netammaula@Neta>
2018-09-12 11:01:40 +0700

    Initial Commit

---
+++ src/jobsheet07/HitungFibonacci.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package jobsheet07;
+
+/**
+ *
+ * @author netammaula
+ */
+public class HitungFibonacci {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void tampilJudul(String identitas) {
+        // TODO code application logic here
+        System.out.println("Identitas :"+ identitas);
+        
+        System.out.println("\nHitung Fibonacci");
+        System.out.println("1, 1, 2, 3, 5, 8, 13, 21, ... dst.\n");
+    }
+    
+        String identitas = "Neta Minkhatul Maula / X RPL 3 / 31";
+        
+        tampilJudul(identitas);
+    
+}
